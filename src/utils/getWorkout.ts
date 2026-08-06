b1a6d8d88abbeff62c06856f3fb451ffd378a363
douchementalexis-sketch <Douchement.alexis@gmail.com>
2026-08-06 23:35:49 +0200

    Retour au calendrier automatique

--- src/utils/getWorkout.ts
+++ src/utils/getWorkout.ts
@@ -8,4 +8,38 @@ export function getWorkout(date: string) {
 
   return workouts.fullBodyA;
 
+  // ===========================
+  // MODE NORMAL
+  // ===========================
+
+  /*
+  const day =
+    new Date(date).getDay();
+
+  switch (day) {
+
+    case 1:
+      return workouts.fullBodyA;
+
+    case 2:
+      return workouts.cardio;
+
+    case 3:
+      return workouts.fullBodyB;
+
+    case 4:
+      return workouts.rest;
+
+    case 5:
+      return workouts.fullBodyC;
+
+    case 6:
+      return workouts.cardio;
+
+    default:
+      return workouts.rest;
+
+  }
+  */
+
 }
