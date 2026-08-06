5208117c7c201993916dc90d9979229b9c835ef2
douchementalexis-sketch <Douchement.alexis@gmail.com>
2026-08-06 23:34:28 +0200

    Ajout des fiches d'exercices illustrées

--- src/utils/getWorkout.ts
+++ src/utils/getWorkout.ts
@@ -2,32 +2,10 @@ import { workouts } from "../data/programs";
 
 export function getWorkout(date: string) {
 
-  const day =
-    new Date(date).getDay();
+  // ===========================
+  // MODE TEST
+  // ===========================
 
-  switch (day) {
-
-    case 1:
-      return workouts.fullBodyA;
-
-    case 2:
-      return workouts.cardio;
-
-    case 3:
-      return workouts.fullBodyB;
-
-    case 4:
-      return workouts.rest;
-
-    case 5:
-      return workouts.fullBodyC;
-
-    case 6:
-      return workouts.cardio;
-
-    default:
-      return workouts.rest;
-
-  }
+  return workouts.fullBodyA;
 
 }
