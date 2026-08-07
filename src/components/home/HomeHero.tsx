d1359aac87249b7ba4279543ed63b6532258afcf
douchementalexis-sketch <Douchement.alexis@gmail.com>
2026-08-07 23:06:56 +0200

    fix(home): suppression des références flowers-left/right

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -2,8 +2,6 @@ import "./HomeHero.css";
 
 import heroBg from "../../assets/hero/hero-bg.png";
 import clouds from "../../assets/hero/clouds.png";
-import flowersLeft from "../../assets/hero/flowers-left.png";
-import flowersRight from "../../assets/hero/flowers-right.png";
 import sparkles from "../../assets/hero/sparkles.png";
 import avatarRachel from "../../assets/hero/avatar-rachel.png";
 
@@ -26,18 +24,6 @@ export default function HomeHero() {
       />
 
       <img
-        src={flowersLeft}
-        alt=""
-        className="heroFlowersLeft"
-      />
-
-      <img
-        src={flowersRight}
-        alt=""
-        className="heroFlowersRight"
-      />
-
-      <img
         src={sparkles}
         alt=""
         className="heroSparkles"
@@ -56,15 +42,11 @@ export default function HomeHero() {
           <div>
 
             <span className="heroHello">
-
               Bonjour
-
             </span>
 
             <h1>
-
               Rachel 🌸
-
             </h1>
 
           </div>
@@ -74,23 +56,17 @@ export default function HomeHero() {
         <div className="heroBottom">
 
           <h2>
-
             Aujourd'hui
-
           </h2>
 
           <p>
-
             Chaque petite victoire compte
-
           </p>
 
         </div>
 
         <div className="heroHeart">
-
           ❤️
-
         </div>
 
       </div>
