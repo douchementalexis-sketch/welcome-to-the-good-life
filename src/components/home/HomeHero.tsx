98738669cd74bf02f4704774e6bdd0682dde5b82
douchementalexis-sketch <Douchement.alexis@gmail.com>
2026-08-07 23:19:53 +0200

    style(home): ajustement du hero

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -1,17 +1,78 @@
-<div className="heroBottom">
+import "./HomeHero.css";
 
-  <h2>
-    Aujourd'hui
-  </h2>
+import heroBg from "../../assets/hero/hero-bg.png";
+import clouds from "../../assets/hero/clouds.png";
+import sparkles from "../../assets/hero/sparkles.png";
+import avatarRachel from "../../assets/hero/avatar-rachel.png";
 
-  <p>
-    Chaque petite victoire compte
-  </p>
+export default function HomeHero() {
 
-</div>
+  return (
 
-<div className="heroHeart">
+    <section className="homeHero">
 
-  ❤️
+      <img
+        src={heroBg}
+        alt=""
+        className="heroBg"
+      />
 
-</div>
+      <img
+        src={clouds}
+        alt=""
+        className="heroClouds"
+      />
+
+      <img
+        src={sparkles}
+        alt=""
+        className="heroSparkles"
+      />
+
+      <div className="heroContent">
+
+        <div className="heroTop">
+
+          <img
+            src={avatarRachel}
+            alt="Rachel"
+            className="heroAvatar"
+          />
+
+          <div>
+
+            <span className="heroHello">
+              Bonjour
+            </span>
+
+            <h1>
+              Rachel 🌸
+            </h1>
+
+          </div>
+
+        </div>
+
+        <div className="heroBottom">
+
+          <h2>
+            Aujourd'hui
+          </h2>
+
+          <p>
+            Chaque petite victoire compte
+          </p>
+
+        </div>
+
+        <div className="heroHeart">
+          ❤️
+        </div>
+
+      </div>
+
+    </section>
+
+  );
+
+}
